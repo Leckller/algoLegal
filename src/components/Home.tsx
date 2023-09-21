8d997f50ece7b81164a0a73be4754d74d3e40a08
Leckller <7leckler@gmail.com>
2023-09-21 01:00:39 -0300

    animacao do h1 concluida + descrições

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,11 @@
 import AnimaisComp from "./AnimaisComp"
+import HomeContent from "./HomeContent"
 import WeatherComp from "./WeatherComp"
 
 function Home ({choseApi}: {choseApi: number, setChoseApi: (p: number) => void}) {
   return(
     <>
+      {choseApi === 0 && <HomeContent/>}
       {choseApi === 1 && <WeatherComp />}
       {choseApi === 2 && <AnimaisComp />}
     </>
---
+++ src/components/HomeContent.tsx
@@ -0,0 +1,19 @@
+function HomeContent() {
+  return (
+    <div>
+          <p>
+            Logo acima temos a barra de navegação é 
+            com ela que você irá se locomover por esse site.
+          </p>
+          <p>
+            Na rota "About" há um breve texto explicando o site.
+          </p>
+          <p>
+            Na rota "Apis" é onde você poderá escolher qual aplicação
+            será renderizada aqui na rota "Home".
+          </p>
+    </div>
+  )
+}
+
+export default HomeContent
